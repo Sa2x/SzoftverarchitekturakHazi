98a0b07241f477f87340960c28126b3ab3b31836
Hadarics Szabolcs <hasza98@gmail.com>
2021-11-27 22:31:51 +0100

    Profile works as /me

--- recipesfrontend/src/app/components/profile/profile.component.ts
+++ recipesfrontend/src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TokenStorageService } from '../../services/token-storage.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +9,19 @@ import { TokenStorageService } from '../../services/token-storage.service';
 })
 export class ProfileComponent implements OnInit {
   currentUser: any;
+  token: any;
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private authService : AuthService, private tokenStorageService : TokenStorageService) { }
 
   ngOnInit(): void {
-    this.currentUser = this.token.getUser();
+    this.token = this.tokenStorageService.getToken();
+    this.authService.self().subscribe(
+      data => {
+        this.currentUser = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 }
