b965d297366e32dee5a3d0ce17862ce62a968c3f
Bálint Sándor <bsanyi1998@gmail.com>
2021-11-28 17:42:02 +0100

    Finished email feature

--- backend/src/main/kotlin/com/example/backend/dto/CreateRecipeDTO.kt
+++ backend/src/main/kotlin/com/example/backend/dto/CreateRecipeDTO.kt
@@ -3,7 +3,7 @@ package com.example.backend.dto
 import org.springframework.web.multipart.MultipartFile
 
 data class CreateRecipeDTO(
-    val file:MultipartFile,
-    val name:String,
-    val description:String
+    val file:MultipartFile?,
+    val name:String?,
+    val description:String?
 )
